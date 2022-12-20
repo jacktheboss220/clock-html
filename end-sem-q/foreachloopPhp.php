17a2eb20874013305b985f7bae4d8e05744e9b14
Mahesh Kumar <jacktheboss2.2.0@gmail.com>
2022-12-20 20:18:07 +0530

    ad

--- end-sem-q/foreachloopPhp.php
+++ end-sem-q/foreachloopPhp.php
@@ -9,6 +9,8 @@
     <h1>Student Details</h1><br>
     <?php
     $std = array(202010101130077, "Mahesh", 8.22);
+    // $s = array("name" => "Mahesh Kumar", "rollNo" => 202010101130077, "CGPA" => 8.22);
+    // echo "Name: " . $s['name'];
     foreach ($std as $val) {
         echo "$val <br>";
     }
