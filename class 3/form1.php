c4e9fa841a5d9248120ef9c319d9abbf704888b7
Mahesh Kumar <jacktheboss2.2.0@gmail.com>
2022-12-20 17:50:53 +0530

    11

--- class 3/form1.php
+++ class 3/form1.php
@@ -8,22 +8,34 @@
     <title>FORM1</title>
 </head>
 
+<?php
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $name = $_POST['name'];
+    $last = $_POST['last'];
+    $option = $_POST['course'];
+    $gender = $_POST['gender'];
+    echo "$name " . " $last " . " $option " . " $gender";
+}
+
+?>
+
 <body>
     <dev class="container" style="text-align: center;">
         <h2><u>Admission Form</u></h2>
         <hr>
-        <form action="" target="_blank">
-            <b>First Name</b><input type="text" class="name" placeholder=""><br><br>
-            <b>Last Name</b><input type="text" class="name" placeholder=""><br><br>
-            <b>Gender</b><input type="radio" name="gender" id="male"><label for="male">Male</label>
-            <input type="radio" name="gender" id="female"><label for="female">Female</label><br><br>
+        <form action="/class%203/form1.php" method="POST">
+            <b>First Name</b><input type="text" class="name" placeholder="" name="name"><br><br>
+            <b>Last Name</b><input type="text" class="name" placeholder="" name="last"><br><br>
+            <b>Gender</b><input type="radio" name="gender" id="male" value="male"><label for="male">Male</label>
+            <input type="radio" name="gender" id="female" value="female"><label for="female">Female</label><br><br>
             <h3>Location</h3>
             <input type="checkbox" name="city" id="Lucknow"><label for="Lucknow">Lucknow</label>
             <input type="checkbox" name="city" id="Delhi"><label for="Delhi">Delhi</label>
             <input type="checkbox" name="city" id="Goa"><label for="Goa">Goa</label>
             <input type="checkbox" name="city" id="Pune"><label for="Pune">Pune</label><br>
             <h3>Select The Course</h3>
-            <select name="" id="course">
+            <select name="course" id="course">
                 <option value="BCA">BCA</option>
                 <option value="B.Teach">B Teach</option>
                 <option value="Civil">Civil</option>
