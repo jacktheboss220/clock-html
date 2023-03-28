6e0927928cdc7a80dc8d8e982a44c1d03d14324d
jacktheboss220 <jacktheboss2.2.0@gmail.com>
2023-03-28 21:46:07 +0530

    kk

--- Google Pixel/script.js
+++ Google Pixel/script.js
@@ -8,8 +8,7 @@ const addTable = (container, tableName, keys, values, checked) => {
         const td1 = document.createElement('td')
         const td2 = document.createElement('td')
         td1.innerText = keys[index]
-        if (checked) td2.innerText = value[1]
-        else td2.innerText = value[0]
+        td2.innerText = checked ? value[1] : value[0]
         tr.appendChild(td1)
         tr.appendChild(td2)
         spanTr.appendChild(td1)
@@ -22,12 +21,12 @@ const addTable = (container, tableName, keys, values, checked) => {
     document.getElementsByClassName(container)[0].append(table)
 }
 
-// setInterval(() => {
-//     console.log('data reloaded')
-//     dataReload()
-// }, 1000 * 60 * 30)
+setInterval(() => {
+    console.log('data reloaded')
+    dataReload()
+}, 1000 * 4)
 
-let chart, chart2
+let chart
 
 let configuration = (keys, values, type) => {
     return {
@@ -39,12 +38,12 @@ let configuration = (keys, values, type) => {
                     label: 'Number of clicks',
                     data: values.map(r => r[0]),
                     backgroundColor: [
-                        'rgba(255, 99, 132, 0.3)',
-                        'rgba(54, 162, 235, 0.3)',
-                        'rgba(255, 206, 86, 0.3)',
-                        'rgba(75, 192, 192, 0.3)',
-                        'rgba(153, 102, 255, 0.3)',
-                        'rgba(255, 159, 64, 0.3)'
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)'
                     ],
                     borderColor: [
                         'rgba(255, 99, 132, 1)',
@@ -54,14 +53,31 @@ let configuration = (keys, values, type) => {
                         'rgba(153, 102, 255, 1)',
                         'rgba(255, 159, 64, 1)'
                     ],
-                    borderWidth: 2
+                    borderWidth: 1.5
                 }
             ]
         },
         options: {
+            animation: {
+                duration: 0
+            },
             scales: {
                 y: {
-                    beginAtZero: true
+                    beginAtZero: true,
+                    ticks: {
+                        font: {
+                            size: 12,
+                            weight: 'bold'
+                        }
+                    }
+                },
+                x: {
+                    ticks: {
+                        font: {
+                            size: 12,
+                            weight: 'bold'
+                        }
+                    }
                 }
             },
             maintainAspectRatio: false
@@ -85,7 +101,7 @@ let configuration = (keys, values, type) => {
     }
 }
 const dataReload = async () => {
-    const data = await fetch(
+    await fetch(
         'https://mr-retail-token.herokuapp.com/sendClicks?client_id=client1&password=password1'
     ).then(res =>
         res.json().then(r => {
@@ -101,14 +117,6 @@ const dataReload = async () => {
             } else {
                 chart = new Chart(ctx, configuration(keys, values, 'bar'))
             }
-
-            // var ctx2 = document.getElementById('myChart2').getContext('2d')
-            // if (chart2) {
-            //     chart2.destroy()
-            //     chart2 = new Chart(ctx2, configuration(keys, values, 'doughnut'))
-            // } else {
-            //     chart2 = new Chart(ctx2, configuration(keys, values, 'doughnut'))
-            // }
         })
     )
 }
